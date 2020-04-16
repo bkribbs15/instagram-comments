4b294a4ef3f6607ddf85804df3aba5b2acb7308b
Bryan Kribbs <bakribbs@us.ibm.com>
2020-04-15 21:39:28 -0500

    Updates

---
+++ precache-manifest.92482f0608ab61b30a82f7d4e3db72bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2b6f917d47b8462c3d6121278b55f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff4cdfddfc916d9814e6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "94c29b60112578043c18",
+    "url": "/static/js/2.fa91c64a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa91c64a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4cdfddfc916d9814e6",
+    "url": "/static/js/main.67e44bc2.chunk.js"
+  },
+  {
+    "revision": "5db23dc1721ea305c89a",
+    "url": "/static/js/runtime-main.159d7a24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
