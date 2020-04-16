fdf1507fdfdf41832280e2f67a5de44210af1549
Bryan Kribbs <bakribbs@us.ibm.com>
2020-04-15 21:54:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
+import props from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="shopping-list">
+      <h1>Shopping List for {props.name}</h1>
+      <ul>
+        <li>Instagram</li>
+        <li>WhatsApp</li>
+        <li>Oculus</li>
+      </ul>
     </div>
   );
 }
